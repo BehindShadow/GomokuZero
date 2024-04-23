ca1ffed3418dbf12e9dcf8c39c0d0062469f1cec
Zeryan <884414918@qq.com>
2024-04-23 19:02:38 +0800

    add code

---
+++ cfg.py
@@ -0,0 +1,10 @@
+import torch
+
+class config():
+    temperature = 1
+    Cpuct = 0.1
+    batch_size = 32
+    board_size = 3
+    learning_rate = 0.02
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
